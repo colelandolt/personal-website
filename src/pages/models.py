12076bf9e7eb825e376fdfbffc10a6e79a23d479
Cole Landolt <colelandolt33@gmail.com>
2024-05-05 19:00:14 -0700

    update email field

--- src/pages/models.py
+++ src/pages/models.py
@@ -3,7 +3,12 @@ from django.core.validators import validate_email
 
 class Subscriber(models.Model):
     name = models.CharField(blank=True, max_length=255)
-    email = models.EmailField(max_length=255, validators=[validate_email])
+    email = models.EmailField(
+        max_length=255,
+        validators=[validate_email],
+        unique=True
+    )
+    active = models.BooleanField(default=True)
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
