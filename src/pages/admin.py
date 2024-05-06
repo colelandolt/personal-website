f04b881b3d709bbf6ba92b6925171e8f442000c8
Cole Landolt <colelandolt33@gmail.com>
2024-05-05 19:00:59 -0700

    update admin view

--- src/pages/admin.py
+++ src/pages/admin.py
@@ -3,9 +3,9 @@ from django.contrib import admin
 from .models import Subscriber
 
 class SubscriberAdmin(admin.ModelAdmin):
-    list_display = ['email', 'name', 'timestamp']
+    list_display = ['email', 'name', 'active', 'timestamp']
     search_fields = ['email', 'name']
     readonly_fields = ['timestamp']
-    list_filter = ['timestamp']
+    list_filter = ['active', 'timestamp']
 
 admin.site.register(Subscriber, SubscriberAdmin)
