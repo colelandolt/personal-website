dec985a695a0dfa27d3cccddc670af4389e646dd
Cole Landolt <colelandolt33@gmail.com>
2024-05-05 17:51:38 -0700

    initialize application with model form

---
+++ src/pages/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.core.validators import validate_email
+
+class Subscriber(models.Model):
+    name = models.CharField(blank=True, max_length=255)
+    email = models.EmailField(max_length=255, validators=[validate_email])
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.email
